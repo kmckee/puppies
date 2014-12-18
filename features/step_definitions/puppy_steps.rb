c17847f3bff0f10016d1b0424ed7f1245dd59978
Kyle McKee <mckee.kyle@gmail.com>
2014-12-18 15:00:46 -0500

    Micro commit.

--- features/step_definitions/puppy_steps.rb
+++ features/step_definitions/puppy_steps.rb
@@ -1,5 +1,7 @@
-Then(/^I should see "(.*?)"$/) do |expected|
-  @current_page.text.should include expected
+Given(/^I am completing an adoption$/) do
+  visit(HomePage).view_details_for
+  on(AdoptionPage).adopt
+  on(CartPage).complete_adoption
 end
 
 When(/^I adopt a puppy$/) do
@@ -9,12 +11,10 @@ When(/^I adopt a puppy$/) do
   on(CheckoutPage).check_out
 end
 
-Given(/^I am completing an adoption$/) do
-  visit(HomePage).view_details_for
-  on(AdoptionPage).adopt
-  on(CartPage).complete_adoption
-end
-
 When(/^I attempt to checkout with no (.*)$/) do |missing_field_name|
   on(CheckoutPage).check_out(missing_field_name => '')
+end
+
+Then(/^I should see "(.*?)"$/) do |expected|
+  @current_page.text.should include expected
 end
